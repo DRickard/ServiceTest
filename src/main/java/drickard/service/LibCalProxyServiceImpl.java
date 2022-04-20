59df9969c85a4b8d7d17abef799621fae8318138
David Rickard <drickard1967@library.ucla.edu>
2022-04-20 16:31:49 +0000

    populating

---
+++ src/main/java/drickard/service/LibCalProxyServiceImpl.java
@@ -0,0 +1,26 @@
+package drickrd.service;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+public class LibCalProxyServiceImpl implements LibCalProxyService {
+
+    LibCalProxyServiceImpl(final Vertx aVertx) {
+    }
+
+    @Override
+    Future<JsonObject> getClientCredentials(final String aAppName) {
+        return null;
+    }
+
+    @Override
+    Future<JsonObject> getAccessToken(final String aClientID, final String aClientSecret) {
+        return null;
+    }
+
+    @Override
+    Future<JsonObject> getLibCalOutput(final String aOUathToken, final String aLibCalQuery) {
+        return null;
+    }
+}
---
+++ src/main/java/drickard/service/package-info.java
@@ -0,0 +1,2 @@
+@io.vertx.codegen.annotations.ModuleGen(groupPackage = "drickard.service", name = "service", useFutures = true)
+package drickard.service;
